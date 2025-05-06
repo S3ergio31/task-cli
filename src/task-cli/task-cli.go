2b0e81586c1cd615e93cacfc522d2771b7e0c949
Sergio Fidelis <sergiofidelis31@hotmail.com>
2025-05-05 23:23:01 -0300

    Load and store todos from/to json. Add Update and remove features

--- src/task-cli/task-cli.go
+++ src/task-cli/task-cli.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"slices"
 	"time"
@@ -27,17 +29,18 @@ const (
 )
 
 type Task struct {
-	id          string
-	description string
-	status      TaskStatus
-	createdAt   string
-	updatedAt   string
+	Id          string
+	Description string
+	Status      TaskStatus
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
 
 var commands = []string{ADD, UPDATE, REMOVE, MARK, LIST}
-var todos = make(map[string]*Task)
+var todos = make(map[string]Task)
 
 func main() {
+	loadTodos()
 	command := command()
 	err := validateCommand(command)
 
@@ -50,6 +53,7 @@ func main() {
 	switch command {
 	case ADD:
 		fmt.Printf("%s\n", "# Action: Adding a new task")
+
 		task, err := add(arguments())
 
 		if err != nil {
@@ -58,18 +62,73 @@ func main() {
 			return
 		}
 
-		message := fmt.Sprintf("# Output: Task added successfully (ID: %s)", task.id)
-		fmt.Printf("%s", message)
+		fmt.Printf("# Output: Task added successfully (ID: %s)", task.Id)
 	case UPDATE:
+		fmt.Printf("# Action: # Updating task\n")
+
+		task, err := update(arguments())
+
+		if err != nil {
+			fmt.Printf("%s", err.Error())
+
+			return
+		}
+
+		fmt.Printf("# Output: Task updated successfully (ID: %s)\n", task.Id)
 	case REMOVE:
+		fmt.Printf("# Action: # Deleting task\n")
+
+		task, err := remove(arguments())
+
+		if err != nil {
+			fmt.Printf("%s", err.Error())
+
+			return
+		}
+
+		fmt.Printf("# Output: Task deleted successfully (ID: %s)\n", task.Id)
 	case MARK:
 	case LIST:
 	}
-
+	saveTodos()
 }
 
 func handleError(err *error) {
 
+}
+
+func loadTodos() {
+	jsonFile, err := os.Open("todos.json")
+
+	if err != nil {
+		fmt.Printf("loadTodos: %s\n", err)
+
+		return
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &todos)
+}
+
+func saveTodos() {
+	bytes, jsonErr := json.Marshal(todos)
+
+	if jsonErr != nil {
+		fmt.Printf("saveTodos: %s\n", jsonErr)
+
+		return
+	}
+
+	writeError := os.WriteFile("todos.json", bytes, 0644)
+
+	if writeError != nil {
+		fmt.Printf("saveTodos: %s\n", writeError)
+
+		return
+	}
 }
 
 /*
@@ -96,32 +155,75 @@ func validateCommand(command string) error {
 /*
  * Domain
  */
-func add(arguments []string) (*Task, error) {
+func add(arguments []string) (Task, error) {
 	if len(arguments) == 0 {
-		return nil, errors.New("# Error: add -> invalid description")
+		return Task{}, errors.New("# Error: add -> invalid description")
 	}
 
 	description := arguments[0]
 
-	task := &Task{
-		id:          uuid.NewString(),
-		description: description,
-		status:      TODO,
-		createdAt:   time.Now().GoString(),
-		updatedAt:   time.Now().GoString(),
+	task := Task{
+		Id:          uuid.NewString(),
+		Description: description,
+		Status:      TODO,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
 	}
 
-	todos[task.id] = task
+	todos[task.Id] = task
 
 	return task, nil
 }
 
-func update(task Task, description string) {
+func update(arguments []string) (Task, error) {
+	if len(arguments) != 2 {
+		return Task{}, errors.New("# Error: update -> missing arguments")
+	}
 
+	taskId := arguments[0]
+	newDescription := arguments[1]
+
+	task, err := find(taskId)
+
+	if err != nil {
+		return Task{}, err
+	}
+
+	task.Description = newDescription
+
+	todos[taskId] = task
+
+	return task, nil
 }
 
-func remove(task Task) {
-	delete(todos, task.id)
+func remove(arguments []string) (Task, error) {
+	if len(arguments) == 0 {
+		return Task{}, errors.New("# Error: remove -> Task id is not defined")
+	}
+
+	taskId := arguments[0]
+
+	task, err := find(taskId)
+
+	if err != nil {
+		return Task{}, err
+	}
+
+	delete(todos, task.Id)
+
+	return task, nil
+}
+
+func find(taskId string) (Task, error) {
+	task, ok := todos[taskId]
+
+	if !ok {
+		message := fmt.Sprintf("# Error: remove -> task with (ID: %s) not found\n", taskId)
+
+		return Task{}, errors.New(message)
+	}
+
+	return task, nil
 }
 
 func mark() {
